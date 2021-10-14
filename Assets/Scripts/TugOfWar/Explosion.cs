246579f2bace82a45f534f798c410eab9306cdb6
owencoopersfx <owencoopersfx@gmail.com>
2021-10-14 01:29:31 -0700

    Add fading light to explosion.

--- Assets/Scripts/TugOfWar/Explosion.cs
+++ Assets/Scripts/TugOfWar/Explosion.cs
@@ -8,6 +8,11 @@ public class Explosion : MonoBehaviour
 
     public float shakeLength, shakePower;
 
+    [SerializeField] Light _light;
+    public Light light { get { return _light; } set { _light = light; } }
+
+    private float startIntensity;
+
     public delegate void ExplosionEvent();
     public event ExplosionEvent OnExplosionStarted;
     public event ExplosionEvent OnExplosionFinished;
@@ -26,16 +31,25 @@ public class Explosion : MonoBehaviour
 
     private void Awake()
     {
+        particleSystem = GetComponent<ParticleSystem>();
+
+        _light = transform.GetChild(0).GetComponent<Light>();
+        startIntensity = _light.intensity;
     }
 
     void Start()
     {
-        particleSystem = GetComponent<ParticleSystem>();
+
     }
 
     private void Update()
     {
         enable = particleSystem.IsAlive();
+
+        if (enable)
+        {
+            light.intensity -= 100 * Time.deltaTime;
+        }
 
         if (!enable)
         {
@@ -51,6 +65,8 @@ public class Explosion : MonoBehaviour
 
     void Explode()
     {
+        light.intensity = startIntensity;
+
         ScreenshakeController.Instance.StartShake(shakeLength, shakePower);
 
         RaiseExplosionStarted();
